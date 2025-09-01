4b886dd1e166aafd86f1503f53f6adcd68137f69
Meenakshi Kumari <meenakshikumari@Meenakshis-MacBook-Air.local>
2025-09-02 01:17:22 +0530

    Created new repo

---
+++ src/config/gemini.js
@@ -0,0 +1,46 @@
+
+
+// To run this code you need to install the following dependencies:
+// npm install @google/genai mime
+// npm install -D @types/node
+
+import {
+  GoogleGenAI,
+} from '@google/genai';
+
+async function main(prompt) {
+  const ai = new GoogleGenAI({
+    apiKey: import.meta.env.VITE_GOOGLE_GEMINI_API
+  });
+  const config = {
+    thinkingConfig: {
+      thinkingBudget: -1,
+    },
+  };
+  const model = 'gemini-2.5-pro';
+  const contents = [
+    {
+      role: 'user',
+      parts: [
+        {
+          text: prompt,
+        },
+      ],
+    },
+  ];
+
+  const response = await ai.models.generateContentStream({
+    model,
+    config,
+    contents,
+  });
+  let fileIndex = 0;
+  let fullText = "";
+  for await (const chunk of response) {
+    console.log(chunk.text);
+    fullText += chunk.text;
+  }
+  return fullText;
+}
+
+export default main;
